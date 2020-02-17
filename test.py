bb6d1d1d7b493a6421c4094f59656bee9daffde7
Yanz <rzolalainayannick@gmail.com>
2020-02-17 11:30:16 -0500

    Naive implementation v3.1

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ seq = ["AB", "BC"]
 def scan_genome_given_seq(genome, seq, window_lenght=3, stride=1, thersehold=2):
 
     """
-    input :
-    - genome
-    - seq
-    - window_lenght
-    - stride
+      input :
+        - genome : string representing the long string which is the genome
+        - motif_list : list of "motif" to check if it exists in a window
+        - window_lenght : length of the window where we must check the "motifs"
+        - stride : window displacement stride
 
-    Returns the window whose number of sequences present is greater than a certain threshold.
+      Returns the window whose number of sequences present is greater than a certain threshold.
     """
 
     list_result = list()
