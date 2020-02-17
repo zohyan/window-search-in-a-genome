71d81bb0dd477d43fb55ed7f1c4ef4f046789a33
Yanz <rzolalainayannick@gmail.com>
2020-02-17 11:22:14 -0500

    Implementation naive-algorithm-v3

--- test.py
+++ test.py
@@ -2,24 +2,36 @@ from Bio import SeqIO
 
 # dict_genome = SeqIO.to_dict(SeqIO.parse("rmark3.fa", "fasta"))
 
-genome = "EWAEWA"
-seq = ["EW", "WA"]
+genome = "ABCABC"
+seq = ["AB", "BC"]
 
-def scan_genome_given_seq(genome, seq, window_lenght=3, stride = 1):
+def scan_genome_given_seq(genome, seq, window_lenght=3, stride=1, thersehold=2):
 
-    dict_result = dict()
+    """
+    input :
+    - genome
+    - seq
+    - window_lenght
+    - stride
+
+    Returns the window whose number of sequences present is greater than a certain threshold.
+    """
+
+    list_result = list()
 
     lenght_result = ((len(genome) - window_lenght)/stride) + 1
 
-    for s in seq:
+    for i in range(int(lenght_result)):
 
-        intermediate_list = []
-        for i in range(int(lenght_result)):
+        nb_motif_checked = 0
+
+        for s in seq:
             if s in genome[i:window_lenght + i]:
-                intermediate_list.append((genome[i:window_lenght + i], i, window_lenght + i))
+                nb_motif_checked += 1
 
-        dict_result[s] = intermediate_list
+        if thersehold == nb_motif_checked:
+            list_result.append((i, window_lenght + i))
 
-    return dict_result
+    return list_result
 
 print(scan_genome_given_seq(genome, seq))
